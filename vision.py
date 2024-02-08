1290d0a754095813a4b0b2e432c5a6be0397806b
AtharvAwle <atharvawle0156@gmail.com>
2024-02-08 21:54:00 +0530

    image input and gemini-pro-vision added

--- vision.py
+++ vision.py
@@ -4,5 +4,34 @@ load_dotenv()
 import streamlit as st 
 import google.generativeai as genai
 import os
+from PIL import Image
 
 genai.configure(api_key=os.getenv("GOOGLR_API_KEY"))
+model = genai.GenerativeModel("gemini-pro-vision")  #use gemini-pro-vision for images
+def getGemini_response(input,image):
+    if input != "":  #if we give some input it will generate the content for text and img
+      response = model.generate_content([input,image])  #generate_content is function which gives us the response.
+    else:
+        response = model.generate_content(image)  #if we dont give text it will generate it through img
+    return response.text
+
+
+st.set_page_config(page_title="Image + Text analise")
+st.header("Gemini Image & Text GPT by ATHARV AWLE")
+
+input = st.text_input("Input prompt: " , key="input")
+
+    
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+        
+
+submit = st.button("Tell me about this img")
+
+if submit:
+    response = getGemini_response(input,image)
+    st.subheader("Response to your prompt....")
+    st.write(response)
